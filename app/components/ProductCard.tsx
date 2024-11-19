1598fa43ad8cf6e4bdfbc22ee8fd7e8fadba7e8f
Jerie See Villanueva <codestackjs007@gmail.com>
2024-11-18 19:10:20 -0800

    Lecture 11.18.24

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+
+
+import AddToCart from "./AddToCart"
+
+const ProductCard = () => {
+    
+  return (
+    // <button onClick={()=>console.log('Clicked')}> Add to Cart</button>
+    <AddToCart/>
+  )
+}
+
+export default ProductCard
