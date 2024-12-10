7684b584bdb8654b59122fe6430d5473e9032ecb
Jerie See Villanueva <codestackjs007@gmail.com>
2024-12-09 21:06:45 -0800

    prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,10 +15,8 @@ const NavBar = () => {
         <Link href= {'/users'} className="mr-5">Users </Link>
         <Link href= {'/admin'} className="mr-5">Admin </Link>
         {status === 'loading' && <div>Loading....</div>}
-        {status === 'authenticated' && <div>{session.user?.name}</div>}
+        {status === 'authenticated' && <div>{session.user?.name} <Link className="ml-3" href={'/api/auth/signout'}> Sign Out</Link></div>  }
        {status === 'unauthenticated' && <Link href= {'/api/auth/signin'} className="mr-5">Login</Link>}
-      
-      
 
     </div>
     
